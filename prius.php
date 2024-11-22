02063b8148237214d92b9955b58410a6f3148167
DanilVolkov228 <volkovdanil20032003@gmail.com>
2024-11-22 18:04:30 +0300

    Create prius.php

---
+++ prius.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Toyota Prius</title>
+</head>
+<body>
+    <header>
+        <h1>Toyota Prius</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">На Главную</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    
+</body>
+</html>

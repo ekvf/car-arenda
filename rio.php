9753efd1bf67d1cb6730fa8de74f661a4fcdba44
DanilVolkov228 <volkovdanil20032003@gmail.com>
2024-11-22 18:07:38 +0300

    Create rio.php

---
+++ rio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Kia Rio</title>
+</head>
+<body>
+    <header>
+        <h1>Kia Rio</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">На Главную</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    
+</body>
+</html>
